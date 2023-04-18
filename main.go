333cd8a8794443a2f4575c9997c093388d76aa28
huangzhenting <378627710@qq.com>
2023-04-18 16:11:16 +0800

    fix: AllowAllOrigins

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/linweiyuan/go-chatgpt-api/api/chatgpt"
 	"github.com/linweiyuan/go-chatgpt-api/api/official"
@@ -25,14 +24,17 @@ func Recover() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func cors(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "*")
+	c.Header("Access-Control-Allow-Headers", "*")
+	c.Next()
+}
+
 func main() {
 	router := gin.Default()
-
-	// 添加CORS中间件
-	config := cors.DefaultConfig()
-	config.AllowAllOrigins = true
-	router.Use(cors.New(config))
-
+	router.Use(cors)
 	router.Use(Recover())
 	router.Use(middleware.HeaderCheckMiddleware())
 
